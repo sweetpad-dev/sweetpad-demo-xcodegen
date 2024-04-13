34632e8f7eb32555ffb022127c71e43a1ddef9e6
Yevhenii Hyzyla <hyzyla@gmail.com>
2024-04-13 22:56:11 +0200

    Initial Commit

---
+++ sweetpad-demo-xcodegen/sweetpad_demo_xcodegenApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweetpad_demo_xcodegenApp.swift
+//  sweetpad-demo-xcodegen
+//
+//  Created by Yevhenii Hyzyla on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct sweetpad_demo_xcodegenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
