6aba6f0063fb22cdf9764950d96f911b8b953125
Yevhenii Hyzyla <hyzyla@gmail.com>
2024-09-22 16:18:47 +0200

    Track xcodeproject

--- sweetpad-demo-xcodegen/ContentView.swift
+++ sweetpad-demo-xcodegen/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let blog = Blog(title: "Hello, world!", content: "This is a blog post")
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            Button("Read more") {
+                print("Button tapped!")
+                print("Blog title: \(blog.title)")
+                
+            }
         }
         .padding()
     }
